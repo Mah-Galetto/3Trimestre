b1be9ad119fdef3408e16882f18322a2943f3acb
Mah-Galetto <127850209+Mah-Galetto@users.noreply.github.com>
2023-11-08 09:40:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();}
+function tocaSom(idElementoAudio) {
+    const elemento= document.querySelector (idElementoAudio).play();
+
+     if (elemento === null);
+     console.log(elemento);
+      alert.play('Elemento não encontrado');
+     }
+    if (elemento !=null && elemento.localName === 'audio'){
+      elemento.play();
+    } else {
+      alert ('Elemento não encontrado');
+    }
+
+    }
+    
 
  const listaDeTeclas = document.querySelectorAll('.tecla')
  
@@ -7,6 +20,7 @@ for (let contador =0; contador < listaDeTeclas.leigth; contador++) {
     const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
 const idAudio =  `#som_ $(instrumento)`;
+
 console.log(idAudio);
     
 tecla.onclick = function () {
@@ -17,7 +31,7 @@ tecla.onclick = function () {
       if (evento.code === 'Enter' || evento.code === 'Space') {
       tecla.classList.add('ativa');
     }
-
+ 
   }
     tecla.onkeyup = function(){
       tecla.classList.remove('ativa');
